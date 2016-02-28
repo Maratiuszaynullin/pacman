7dbf0061df251f29d05fa3ecb201502c71504ba7
Maratiuszaynullin@icloud.com <Maratiuszainullin@icloud.com>
2016-02-28 04:42:49 +0300

    Улучшил классы призраков

--- dynamic_object.py
+++ dynamic_object.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from map import *
+from main import *
 
 
 def is_solid_wall(x, y):
@@ -11,7 +12,7 @@ def is_fragile_wall(x, y):
 
 
 def is_wall(x, y):
-    return isinstance(MAP.data[int(y)][int(x)], FragileWall or SolidWall)
+    return is_solid_wall(x, y) or is_fragile_wall(x, y)
 
 
 """def ghost_wall_react(x, y, v):
@@ -79,41 +80,33 @@ class BlindGhost(DynamicObject):
             self.direction = random.randint(1, 4)
 
         if self.direction == 1:
-            if not (is_solid_wall(self.x + self.velocity, self.y) or is_fragile_wall(self.x + self.velocity, self.y)):
+            if not is_wall(self.x + self.velocity, self.y):
                 self.x += self.velocity
-            else:
-                self.direction = random.choice((2, 3, 4))
-
+            else: self.direction = random.choice((2, 3, 4))
             if self.x >= map_size - 1:
                 self.x = map_size - 1
                 self.direction = random.randint(1, 4)
 
         if self.direction == 2:
-            if not (is_solid_wall(self.x, self.y + self.velocity) or is_fragile_wall(self.x, self.y + self.velocity)):
+            if not is_wall(self.x, self.y + self.velocity):
                 self.y += self.velocity
-            else:
-                self.direction = random.choice((1, 3, 4))
-
+            else: self.direction = random.choice((1, 3, 4))
             if self.y >= map_size - 1:
                 self.y = map_size - 1
                 self.direction = random.randint(1, 4)
 
         if self.direction == 3:
-            if not (is_solid_wall(self.x - self.velocity, self.y) or is_fragile_wall(self.x - self.velocity, self.y)):
+            if not is_wall(self.x - self.velocity, self.y):
                 self.x -= self.velocity
-            else:
-                self.direction = random.choice((1, 2, 4))
-
+            else: self.direction = random.choice((1, 2, 4))
             if self.x <= 0:
                 self.x = 0
                 self.direction = random.randint(1, 4)
 
         if self.direction == 4:
-            if not (is_solid_wall(self.x, self.y - self.velocity) or is_fragile_wall(self.x, self.y - self.velocity)):
+            if not is_wall(self.x, self.y - self.velocity):
                 self.y -= self.velocity
-            else:
-                self.direction = random.choice((1, 2, 3))
-
+            else: self.direction = random.choice((1, 2, 3))
             if self.y <= 0:
                 self.y = 0
                 self.direction = random.randint(1, 4)
@@ -128,49 +121,58 @@ class UnblindedGhost(DynamicObject):
         self.velocity = 4.0 / 10.0
 
     def ghost_AI(self):
-        pass
+        if self.x == pacman.x:
+            for i in range(int(abs(self.y - pacman.y))+1):
+                if self.y > pacman.y:
+                    if is_wall(self.x, self.y - i): break
+                    else: return 'up'
+                else:
+                    if is_wall(self.x, self.y + i): break
+                    else: return 'down'       #elif ghost_fear: self.direction = up // else: self.direction == down
+        if self.y == pacman.y:
+            for i in range(int(abs(self.x - pacman.x))):
+                if self.x > pacman.x:
+                    if is_wall(self.x - i, self.y): break
+                    else: return 'left'
+                else:
+                    if is_wall(self.x + i, self.y): break
+                    else: return 'right'
 
     def game_tick(self):
         super(UnblindedGhost, self).game_tick()
         if self.tick % 20 == 0 or self.direction == 0:
             self.direction = random.randint(1, 4)
 
-        if self.direction == 1:
-            if not (is_solid_wall(self.x + self.velocity, self.y) or is_fragile_wall(self.x + self.velocity, self.y)):
-                self.x += self.velocity
-            else:
-                self.direction = random.choice((2, 3, 4))
+        if self.ghost_AI(): self.direction = self.ghost_AI()
 
+        if self.direction == 1:
+            if not is_wall(self.x + self.velocity, self.y):
+                self.x += self.velocity
+            else: self.direction = random.choice((2, 3, 4))
             if self.x >= map_size - 1:
                 self.x = map_size - 1
                 self.direction = random.randint(1, 4)
 
         if self.direction == 2:
-            if not (is_solid_wall(self.x, self.y + self.velocity) or is_fragile_wall(self.x, self.y + self.velocity)):
+            if not is_wall(self.x, self.y + self.velocity):
                 self.y += self.velocity
-            else:
-                self.direction = random.choice((1, 3, 4))
-
+            else: self.direction = random.choice((1, 3, 4))
             if self.y >= map_size - 1:
                 self.y = map_size - 1
                 self.direction = random.randint(1, 4)
 
         if self.direction == 3:
-            if not (is_solid_wall(self.x - self.velocity, self.y) or is_fragile_wall(self.x - self.velocity, self.y)):
+            if not is_wall(self.x - self.velocity, self.y):
                 self.x -= self.velocity
-            else:
-                self.direction = random.choice((1, 2, 4))
-
+            else: self.direction = random.choice((1, 2, 4))
             if self.x <= 0:
                 self.x = 0
                 self.direction = random.randint(1, 4)
 
         if self.direction == 4:
-            if not (is_solid_wall(self.x, self.y - self.velocity) or is_fragile_wall(self.x, self.y - self.velocity)):
+            if not is_wall(self.x, self.y - self.velocity):
                 self.y -= self.velocity
-            else:
-                self.direction = random.choice((1, 2, 3))
-
+            else: self.direction = random.choice((1, 2, 3))
             if self.y <= 0:
                 self.y = 0
                 self.direction = random.randint(1, 4)
